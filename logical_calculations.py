d3f6ffc06aed60e419d52d3ef23b94147c69e80a
trew12 <antonvolodin67@yandex.ru>
2022-10-24 06:06:29 +0300

    add some logical calculations

--- logical_calculations.py
+++ logical_calculations.py
@@ -4,3 +4,5 @@ from logical_operations import logical_or, logical_and, logical_not
 print(logical_or(True, False))
 print(logical_not(True))
 print(logical_and(True, False))
+print(logical_not(logical_and(True, False)))
+print(logical_not(logical_or(True, False)))
